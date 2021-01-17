3273c476edf7481c7dc8bf78ca4a8893903a329b
Alejandro S <alejandro@jreviews.com>
2021-01-16 20:13:33 -0400

    Tests for component registration

--- tests/Unit/AnonymousComponentTest.php
+++ tests/Unit/AnonymousComponentTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Clickfwd\Yoyo\ComponentManager;
 use Clickfwd\Yoyo\Exceptions\ComponentNotFound;
 use function Tests\initYoyo;
 use function Tests\render;
@@ -19,4 +20,9 @@ test('discovers and renders anonymous foo component', function () {
 
 test('updates anonymous foo component', function () {
     expect(update('foo'))->toContain('Bar');
+});
+
+test('registered anonymous component is loaded', function () {
+    ComponentManager::registerComponent('registered-anon');
+    expect(render('registered-anon'))->toContain('id="registered-anon"');
 });
