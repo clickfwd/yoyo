d6652df0fa731b001b7906c6444172e3c823ff98
Alejandro S <alejandro@jreviews.com>
2021-02-28 13:21:11 -0400

    Update tests

--- tests/Feature/NamespacedComponentTest.php
+++ tests/Feature/NamespacedComponentTest.php
@@ -4,13 +4,13 @@ use Clickfwd\Yoyo\Yoyo;
 use Tests\App\Resolvers\BladeComponentResolver;
 use function Tests\render;
 
-test('render namespaced anonymous component', function () {
+it('can render namespaced anonymous component', function () {
     $view = Yoyo::getInstance()->getViewProvider();
     $view->addNamespace('packagename', __DIR__.'/../app-another/views');
     expect(render('packagename::foo'))->toContain('other foo from another app');
 });
 
-test('can render namespaced dynamic component', function () {
+it('can render namespaced dynamic component', function () {
     $yoyo = Yoyo::getInstance();
     $view = $yoyo->getViewProvider();
     $view->addNamespace('packagename', __DIR__.'/../app-another/views');
@@ -18,7 +18,18 @@ test('can render namespaced dynamic component', function () {
     expect(render('packagename::counter', ['count' => 3]))->toContain('The count is now 3');
 });
 
-test('can render namespaced dynamic component with custom resolver', function () {
+it('can render namespaced anonymous component with custom resolver', function () {
+    $yoyo = Yoyo::getInstance();
+    $yoyo->container()->flush();
+    $yoyo->registerComponentResolver(new BladeComponentResolver());
+    $view = $yoyo->getViewProvider('blade');
+    $view->addNamespace('packagename', __DIR__.'/../app-another/views');
+    expect(render('packagename::foo', [
+        'yoyo:resolver' => 'blade',
+    ]))->toContain('blade foo from another app');
+});
+
+it('can render namespaced dynamic component with custom resolver', function () {
     $yoyo = Yoyo::getInstance();
     $yoyo->container()->flush();
     $yoyo->registerComponentResolver(new BladeComponentResolver());
